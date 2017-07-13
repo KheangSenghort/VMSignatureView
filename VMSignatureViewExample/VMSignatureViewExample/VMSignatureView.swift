fe3d5839bf75e2448196beb206ce31271865de97
Vaibhav Misra <vaibhavmisra@vaibhavs-macbook-pro.local>
2017-07-13 09:46:04 +0530

    Changes for smoother signature drawing using quadratic bezier path

--- VMSignatureViewExample/VMSignatureViewExample/VMSignatureView.swift
+++ VMSignatureViewExample/VMSignatureViewExample/VMSignatureView.swift
@@ -30,12 +30,12 @@ class VMSignatureView: UIView {
         }
     }
     
-    // Computed Property returns true if the view actually contains a signature
     public var hasSignature = false
     
     // MARK: - Private properties
     private var path = UIBezierPath()
     private var incrementalImage: UIImage?
+    private var points =  [CGPoint](repeating: CGPoint.zero, count: 4)
     private var ptsCountAfterFirstTouch = 0
     
     // MARK: - Init
@@ -64,49 +64,63 @@ class VMSignatureView: UIView {
     //MARK: - Touches methods
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        self.ptsCountAfterFirstTouch = 0
         if let touch: UITouch = touches.first {
-            let touchPoint: CGPoint = touch.location(in: self)
-            self.path.move(to: touchPoint)
-            self.ptsCountAfterFirstTouch = 0
+            self.points[self.ptsCountAfterFirstTouch] = touch.location(in: self)
         }
         
-        if let delegate = self.delegate {
-            delegate.startedDrawing()
-        }
+        self.delegate?.startedDrawing()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         if let touch: UITouch = touches.first {
-            let touchPoint: CGPoint = touch.location(in: self)
-            self.path.addLine(to: touchPoint)
+            let point = touch.location(in: self)
             self.ptsCountAfterFirstTouch += 1
-            self.setNeedsDisplay()
+            self.points[self.ptsCountAfterFirstTouch] = point
+            if self.ptsCountAfterFirstTouch == 3 {
+                //Take 3rd pt as average of 2nd and 4th pt
+                let curveEndPointX = (self.points[1].x + self.points[3].x)/2
+                let curveEndPointY = (self.points[1].y + self.points[3].y)/2
+                self.points[2] = CGPoint(x: curveEndPointX,
+                                         y: curveEndPointY)
+                //Draw a curve from 1st to 3rd pt with 2nd pt as control pt
+                self.path.move(to: self.points[0])
+                self.path.addQuadCurve(to: self.points[2],
+                                       controlPoint: self.points[1])
+                self.setNeedsDisplay()
+                //Make 3rd pt as 1st pt and 4th pt as 2nd pt for next curve
+                self.points[0] = self.points[2]
+                self.points[1] = self.points[3]
+                self.ptsCountAfterFirstTouch = 1
+            }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch: UITouch = touches.first {
-            let lastPoint: CGPoint = touch.location(in: self)
-            if self.ptsCountAfterFirstTouch == 0 {
-                self.path.addArc(withCenter: lastPoint,
-                                 radius: self.strokeWidth/2,
-                                 startAngle: 0,
-                                 endAngle: CGFloat(Double.pi * 2),
-                                 clockwise: true)
-            }
-            else {
-                self.path.addLine(to: lastPoint)
-            }
-            self.drawBitmap()
-            self.setNeedsDisplay()
-            self.path.removeAllPoints()
-            self.hasSignature = true
+        if self.ptsCountAfterFirstTouch == 0 { // Draw point for single tap
+            self.path.addArc(withCenter: self.points[0],
+                             radius: self.strokeWidth/2,
+                             startAngle: 0,
+                             endAngle: CGFloat(Double.pi * 2),
+                             clockwise: true)
         }
+        else if self.ptsCountAfterFirstTouch == 1 { //Draw a line in case of only 2 pts
+            self.path.move(to: self.points[0])
+            self.path.addLine(to: self.points[1])
+        }
+        else if self.ptsCountAfterFirstTouch == 2 { //Draw a quadratic curve incase of 3 pts
+            self.path.move(to: self.points[0])
+            self.path.addQuadCurve(to: self.points[2],
+                                   controlPoint: self.points[1])
+        }
+        self.drawBitmap()
+        self.setNeedsDisplay()
+        self.path.removeAllPoints()
+        self.ptsCountAfterFirstTouch = 0
         
-        if let delegate = self.delegate {
-            delegate.finishedDrawing()
-        }
+        self.hasSignature = true
+        self.delegate?.finishedDrawing()
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
